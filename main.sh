d1163219fb38cee0359a02cc20cb3e4aea9c85cc
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-03-24 15:37:37 +0530

    add required sh files

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+REPO_URL="https://raw.githubusercontent.com/mahadikbs.ej/required-software/main"
+
+echo "Installing Docker..."
+curl -sSL "$REPO_URL/docker.sh" | bash
+
+echo "Installing Kubernetes..."
+curl -sSL "$REPO_URL/k8s.sh" | bash
+
+echo "Installing Minikube..."
+curl -sSL "$REPO_URL/minikube.sh" | bash
+
+echo "All software installed successfully!"
