f57466b6599014620f8c49f20f1ac33f47f47bfb
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-15 15:30:57 +0530

    add kind

--- custom/kind.sh
+++ custom/kind.sh
@@ -1,5 +1,6 @@
 # set -e
 REPO_URL="https://raw.githubusercontent.com/mahadikbs/required-software/main/custom/"
-echo "Installing Docker..."
-cat $REPO_URL/docker.sh
-curl -sSL "$REPO_URL/docker.sh" | bash
+echo "Installing Software..."
+cat $REPO_URL/kind_amzn_lnx.sh
+
+curl -sSL "$REPO_URL/kind_amzn_lnx.sh" | bash
