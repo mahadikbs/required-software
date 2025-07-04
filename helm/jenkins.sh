d05a7c39e87e1152edaf3d03fc6e3994d7929fd4
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-07-04 13:03:07 +0530

    Update jenkins with helm

--- helm/jenkins.sh
+++ helm/jenkins.sh
@@ -3,6 +3,4 @@ kubectl create namespace jenkins
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
 
-helm install jenkins jenkins/jenkins -n jenkins \
-  --set server.service.type=NodePort \
-  --set server.service.nodePort=35000
+helm install jenkins jenkins/jenkins -n jenkins -f values.yaml \
